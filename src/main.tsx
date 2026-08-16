b1cafe5018479de2bd2e3b07d27623e8ed8692c7
KaraboMatsemela1 <matsemelakarabo@gmail.com>
2026-08-16 15:48:19 +0200

    feat: wire smooth navigation transitions

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App'
+import { setupNavigationMotion } from './lib/navigation-motion'
 import { applyRuntimeSeo } from './lib/seo'
 import './styles/index.css'
 import './styles/gallery-carousel.css'
 import './styles/polish.css'
 import './styles/profile-content.css'
 import './styles/logo-preview.css'
+import './styles/navigation-motion.css'
 
 applyRuntimeSeo()
 
@@ -15,3 +17,9 @@ createRoot(document.getElementById('root')!).render(
     <App />
   </React.StrictMode>,
 )
+
+const cleanupNavigationMotion = setupNavigationMotion()
+
+if (import.meta.hot) {
+  import.meta.hot.dispose(cleanupNavigationMotion)
+}
