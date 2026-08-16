aee44c146d35511666b5532dd6e69405cee27420
KaraboMatsemela1 <matsemelakarabo@gmail.com>
2026-08-16 17:46:25 +0200

    feat: mount donation section in site

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App'
+import DonationPortal from './DonationPortal'
 import { applyRuntimeSeo } from './lib/seo'
 import './styles/index.css'
 import './styles/gallery-carousel.css'
@@ -8,11 +9,13 @@ import './styles/polish.css'
 import './styles/profile-content.css'
 import './styles/logo-preview.css'
 import './styles/lovable-motion.css'
+import './styles/donation.css'
 
 applyRuntimeSeo()
 
 createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
+    <DonationPortal />
   </React.StrictMode>,
 )
